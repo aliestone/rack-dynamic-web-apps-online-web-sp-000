0c79d83edb43f1fc6287b798a31d33bcf01fd4a1
Alie Stone <helloaliestone@gmail.com>
2020-12-13 22:13:06 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -3,8 +3,8 @@ class Application
       resp = Rack::Response.new
 
       num_1 = Kernel.rand(1..2)
-      num_2 = Kernel.rand(1..20)
-      num_3 = Kernel.rand(1..20)
+      num_2 = Kernel.rand(1..2)
+      num_3 = Kernel.rand(1..2)
 
       if num_1==num_2 && num_2==num_3
         resp.write "You Win"
